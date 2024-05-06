6388215637ce55d0644aa34c24d9fbcdeda06df2
sonaly-mehr <sonaly.mehr@gmail.com>
2024-05-06 23:42:41 +0600

    updated

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -20,7 +20,7 @@ const globalErrorHandler: ErrorRequestHandler = (
 ) => {
   config.env === 'development'
     ? console.log(`🐱‍🏍 globalErrorHandler ~~`, { error })
-    : errorlogger.error(`🐱‍🏍 globalErrorHandler ~~`, error);
+    : console.log(`🐱‍🏍 globalErrorHandler ~~`, error);
 
   let statusCode = 500;
   let message = 'Something went wrong !';
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { errorlogger } from './shared/logger';
 import { RedisClient } from './shared/redis';
 
 process.on('uncaughtException', error => {
-  errorlogger.error(error);
+  console.log(error);
   process.exit(1);
 });
 
@@ -22,21 +22,21 @@ async function bootstrap() {
 
 
     await mongoose.connect(config.database_url as string);
-    // logger.info(`🛢   Database is connected successfully`);
+    // console.log(`🛢   Database is connected successfully`);
     console.log(`🛢   Database is connected successfully`);
 
     server = app.listen(config.port, () => {
-      // logger.info(`Application  listening on port ${config.port}`);
+      // console.log(`Application  listening on port ${config.port}`);
       console.log(`Application  listening on port ${config.port}`);
     });
   } catch (err) {
-    errorlogger.error('Failed to connect database', err);
+    console.log('Failed to connect database', err);
   }
 
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorlogger.error(error);
+        console.log(error);
         process.exit(1);
       });
     } else {
@@ -48,7 +48,7 @@ async function bootstrap() {
 bootstrap();
 
 // process.on('SIGTERM', () => {
-//   logger.info('SIGTERM is received');
+//   console.log('SIGTERM is received');
 //   if (server) {
 //     server.close();
 //   }
